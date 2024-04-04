30571d62467da95ec2cb46d881f8b9e2e9a47e85
KaurGR <kaur-gabriel.rajasalu@tktk.ee>
2024-04-04 08:29:31 +0300

    Create new app

---
+++ RajasaluFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+Console asks for name
+user input(1)
+Console responds Hello "input(1)"
+*/
+
+//Variables
+string a;
+
+Console.WriteLine("Hello, What's your name?");
+a = Console.ReadLine();
+//string a=Console.ReadLine();
+
+//Console.WriteLine("Hello, " + a);
+Console.WriteLine($"Hello {a}! ");
